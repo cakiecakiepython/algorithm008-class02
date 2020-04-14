b38c80d652cec3683535f679eb977db8b9365637
Ian Miao <ian.miao@liulishuo.com>
2020-04-14 22:13:58 +0800

    merge two sorted lists

---
+++ Week_01/mergetwosortedlists.go
@@ -0,0 +1,37 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	dummyNode := &ListNode{}
+	head := dummyNode
+
+	for l1 != nil && l2 != nil {
+
+		if l1.Val <= l2.Val {
+			head.Next = l1
+			l1 = l1.Next
+		} else {
+			head.Next = l2
+			l2 = l2.Next
+		}
+
+		head = head.Next
+	}
+
+	if l1 != nil {
+		head.Next = l1
+	}
+
+	if l2 != nil {
+		head.Next = l2
+	}
+
+	return dummyNode.Next
+}
